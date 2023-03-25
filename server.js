9abb2976682ed26469af838ee9b2d70d99376df3
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2023-03-25 14:35:39 -0400

    # 20230325 ewj moved model to trainable GPT-3-turbo; demand=small,internal

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 require('dotenv').config()
 const openai = require('openai')
-const apiKey = process.env.OPENAI_API_KEY
-// Set up a GPT-3 instance
-const instance = new openai.GPT3({
-	apiKey,
-})
-// Call the GPT-3 API
-instance.complete({
-		prompt: 'How do I train you on MyLife corporate materials?',
-		maxTokens: 5,
+const api_key = process.env.OPENAI_API_KEY
+const api_endpoint = 'https://api.openai.com/v1/models/text-davinci-002'
+const prompt = 'How do I train you on MyLife corporate materials?'
+const max_tokens = process.env.OPENAI_MAX_TOKENS
+const temperature = process.env.OPENAI_TEMP
+// Set up a GPT-3 turbo instance
+const openai_instance = new openai(
+	api_key,
+	{
+		apiKey: api_key,
+		model: api_endpoint,
+	},
+)
+openai_instance.complete({
+		prompt: prompt,
+		maxTokens: max_tokens,
+		temperature: temperature,
 })
 	.then(res => {
 		console.log(res.data.choices[0].text)
