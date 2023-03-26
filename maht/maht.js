61954e69807fe86c9434612a58404d6d7a22f006
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2023-03-26 01:49:22 -0400

    # 20230325 ewj cosmetic

--- maht/maht.js
+++ maht/maht.js
@@ -9,15 +9,13 @@ const config = new Configuration({
 	timeoutMs: process.env.OPENAI_TIMEOUT,
 	basePath: process.env.OPENAI_BASE_URL,
 })
-console.log('here',process.env.OPENAI_API_KEY)
 const openai = new OpenAIApi(config)
-
+//	public functions
 async function processRequest(_question){
 	let question=''	//	enter any dynamic interrupt here
 	if(question.length){
 		_question=question
 	}
-
 	// Setting values for the prompt and message to be used in GPT-3.5-Turbo
 	const GPT35TurboMessage = [
 		{ role: "system", content: `Maht (so named by Chappy-G [aka ChatGPT]), the ai-agent assistant to the Board of Directors of MyLife, nonprofit member organization to help protect and remember the 21st century human experience by providing humanity with a free, equitable and secure network of personal archives and narrative legacies that proscribe a digital self; codebase of Maht available publicly on github: ${process.env.OPENAI_MAHT_GITHUB }, paying special attention to root README ## About Maht` },
