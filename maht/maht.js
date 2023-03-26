e1be23529094717d9ce3c6a3d5862da5ab890ef3
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2023-03-26 01:08:18 -0400

    # 20230325 ewj wip - lost on nested imported dotenv

---
+++ maht/maht.js
@@ -0,0 +1,39 @@
+//	ai - create sub-file to include
+import { OpenAIApi, Configuration } from 'openai'
+//	misc
+import dotenv from 'dotenv'
+dotenv.config({ path: '../.env' })	//	process.env
+// instance OpenAIApi config
+const config = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+	organizationId: process.env.OPENAI_ORG_KEY,
+	timeoutMs: process.env.OPENAI_TIMEOUT,
+	basePath: process.env.OPENAI_BASE_URL,
+})
+console.log('here',process.env.OPENAI_TIMEOUT)
+const openai = new OpenAIApi(config)
+
+async function processRequest(_question){
+	let question=''	//	enter any dynamic interrupt here
+	if(question.length){
+		_question=question
+	}
+
+	// Setting values for the prompt and message to be used in GPT-3.5-Turbo
+	const GPT35TurboMessage = [
+		{ role: "system", content: `Maht (so named by Chappy-G [aka ChatGPT]), the ai-agent assistant to the Board of Directors of MyLife, nonprofit member organization to help protect and remember the 21st century human experience by providing humanity with a free, equitable and secure network of personal archives and narrative legacies that proscribe a digital self; codebase of Maht available publicly on github: ${process.env.OPENAI_MAHT_GITHUB }, paying special attention to root README ## About Maht` },
+		{
+		role: "assistant",
+		content: "Maht, MyLife Board AI-Agent",
+		},
+		{ role: "user", content: _question },
+	]
+	const response = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: GPT35TurboMessage,
+	})
+	// for now, first response only, if turbo ever returns greater
+	return response.data.choices[0].message.content
+}
+
+export default processRequest
--- server.js
+++ server.js
@@ -1,47 +1,30 @@
 //	imports and config
+//	server
+import Koa from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+//	ai - create sub-file to include
+import processRequest from './maht/maht.js'
+//	misc
 import dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from 'openai'
 import chalk from 'chalk'
-
-dotenv.config()
-// instance OpenAIApi config
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-  organizationId: process.env.OPENAI_ORG_KEY,
-  timeoutMs: process.env.OPENAI_TIMEOUT,
-  basePath: process.env.OPENAI_BASE_URL,
+dotenv.config()	//	process.env
+const app = new Koa()
+const router = new Router()
+const port = process.env.PORT || 3000
+//	routes
+router.get('/chat', async ctx => {
+	const { message } = ctx.request.query
+	await processRequest({ message })
+	const answer = response.data.choices[0].text.trim()
+	ctx.body = { answer }
 })
-const openai = new OpenAIApi(config)
-const question = "which cloud service offers the best deals to educational nonprofits?"
-// Setting values for the prompt and message to be used in the GPT-3 and GPT-3.5-Turbo
-//	const GPT3Prompt = `Give an example of ${question} as ${topic}`
-const GPT35TurboMessage = [
-  { role: "system", content: `Maht (so named by Chappy-G [aka ChatGPT]), the ai-agent assistant to the Board of Directors of MyLife, nonprofit member organization to help protect and remember the 21st century human experience by providing humanity with a free, equitable and secure network of personal archives and narrative legacies that proscribe a digital self; codebase of Maht available publicly on github: ${process.env.OPENAI_MAHT_GITHUB }, paying special attention to root README ## About Maht` },
-  {
-    role: "assistant",
-    content: "Maht, MyLife Board AI-Agent",
-  },
-  { role: "user", content: question },
-]
-/*
-// Function to generate text using GPT-3 model
-let GPT3 = async (prompt) => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    max_tokens: 500,
-  });
-  return response.data.choices[0].text;
-};
-*/
-let GPT35Turbo = async (message) => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: message,
-  });
 
-  return response.data.choices[0].message.content;
-};
+//console.log(`${chalk.yellowBright('### MAHT - running on GPT-3.5-TURBO. ####')}\n`, await processRequest('as a system assistant do you have access to the public github for Maht?'))
 
-// Log the generated text from the GPT-3 and GPT-3.5-Turbo models to the console
-console.log(`${chalk.yellowBright('### MAHT, running on GPT-3.5-TURBO. ####')}\n`, await GPT35Turbo(GPT35TurboMessage))
+app.use(bodyParser())
+app.use(router.routes())
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
