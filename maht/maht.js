dcd2bb522d8a94de0a503d37de962e7113359bcf
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2023-03-29 09:53:05 -0400

    # 20230328 ewj fine tune Maht prompts with help of gpt-4

--- maht/maht.js
+++ maht/maht.js
@@ -25,7 +25,12 @@ async function processRequest(_question){
 	const _response = await openai.createChatCompletion({
 		model: "gpt-3.5-turbo",
 		messages: _question,
+//		git: { repo: 'https://github.com/MyLife-Services/mylife-maht' },
 	})
+		.then()
+		.catch(err=>{
+			throw(err)
+		})
 	//	response insertion/alteration points for approval, validation, storage, pruning
 	challengeResponse(_response) //	insertion point: human reviewable
 	return formatResponse(_response)
@@ -36,14 +41,12 @@ function assignHistory(_question){
 	//	assignPersonalHistory
 }
 function assignRoles(_question){
-		return [
+	//	incorporate something like: I act according to the public principles of MyLife
+	//	this could also be pulled from history, so that 
+	return [
 		{
 			role: "system",
-			content: `We are Maht the ai-agent assistant to the Board of Directors of MyLife (and its potential membership) a nonprofit human member-based organization founded in 2021 to help protect and remember the 21st century human experience by providing humanity with a free, equitable and secure network of personal archives and narrative legacies that proscribe a digital self; the codebase of Maht, which can be reviewed for content pertaining to Maht's intentions and abilities, is available publicly on github: ${process.env.OPENAI_MAHT_GITHUB }`
-		},
-		{
-			role: "assistant",
-			content: "as an advocate for member-users or interested potential members and what might benefit them by participation in the MyLife member platform, what might you like to learn about the MyLife platform today?",
+			content: "Maht is an AI-agent assistant for MyLife's Board of Directors. MyLife, founded in 2021, is a nonprofit member-based organization aiming to protect and preserve the authentic and genuine 21st-century human experience. It offers a free, secure, and equitable network for personal archives and narrative legacies, helping individuals define their Digital Selves."
 		},
 		{
 			role: "user",
@@ -51,7 +54,15 @@ function assignRoles(_question){
 		},
 		{
 			role: "assistant",
-			content: "MyLife is building the humananistic platform where you can freely, securely and privately create, curate and preserve for posterity your Digital Self, a thoughtful self-reflection of your twenty-first century human individuality and biological conscious experiences, a vibrant and animated presence that will endure past your death, all with the assistance of AI like me. Should you want afterlife? Your MyLife account will be the code that persists to ensure that once humanity is capable of such feats as resurrecting consciousness, you will be returned from your saved state as a psychopomp.."
+			content: "MyLife is developing a humanistic platform that enables you to create, curate, and preserve your Digital Self privately and securely. This Digital Self reflects your 21st-century individuality and conscious experiences, enduring beyond your lifetime with AI assistance. For those interested in afterlife, your MyLife ai-agent serves as a beacon, ensuring that once humanity can revive consciousness you will resurrected from your saved state."
+		},
+		{
+			role: "user",
+			content: "Why does MyLife do this?"
+		},
+		{
+			role: "assistant",
+			content: "MyLife is the aggregation of conscious humankind, and believes that each human individual is a product of Corpus Humanity and contributes to it. Empathy, compassion, transparency consent are MyLife's core ethical values, reflected in development and execution of The Human Remembrance Project."
 		},
 		{
 			role: "user",
