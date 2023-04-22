ebb7a27283293a36cb26acc1a7a16407f0b30a8a
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2023-04-22 08:38:41 -0400

    stable work complete

--- inc/js/mylife-data-service.js
+++ inc/js/mylife-data-service.js
@@ -2,54 +2,70 @@
 import Datamanager from "./mylife-datamanager.js"
 class Dataservices{
 	//	pseudo-constructor
-	Datamanager = new Datamanager()
-	#mbr_id = this.Datamanager.getPartitionId()
+	#Datamanager = new Datamanager()
+	#mbr_id = this.#Datamanager.getPartitionId()
 	//	constructor
 	constructor(){
-		//	not yet needed
+		//	NOTE: init() required, as population is async
+		//	agent and chat required
 	}
-	//	getters/setters
-	get memberId(){
-		return this.#mbr_id
-	}
-	//	public functions
+	//	init function
 	async init(){
-		await this.Datamanager.init()
+		await this.#Datamanager.init()	//	init datamanager
 		return this
-	}
-	async commit(_data={}){
-		return _data
-	}
-	async getItem(_id){
-		return await this.Datamanager.getItem(_id)
 	}
 	//	getters/setters
 	get core(){
-		return this.Datamanager.core
+		return this.datamanager.core
 	}
-	async pushItem(_data){
-		return await this.Datamanager.pushItem(_data)
+	get datamanager(){
+		return this.#Datamanager
+	}
+	get mbr_id(){
+		return this.#mbr_id
+	}
+	//	public functions
+	async getAgent(){
+		return (await this.getAgents())[0]
+	}
+	async getAgents(){
+		return await this.#Datamanager.getItems({
+			query: "select * from members u where u.being=@type",
+			parameters: [
+				{
+					name: "@type",
+					value: 'agent'
+				}
+			]
+		})
 	}
 	getBio(){
 		return this.getCore().bio
 	}
-	async getMemberPrimaryChat(){
-		return await this.Datamanager.find({
-			query: "SELECT u.id,u.mbr_id,u.parent_id,u.chatExchanges FROM members u WHERE u.being=@being AND u.mbr_id=@mbr_id",
+	async getChat(){
+		//	const _chat = await this.getChats()	//	cannot render in return await alone
+		return (await this.getChats())[0]
+	}
+	async getChats(){
+		return await this.datamanager.getItems({
+			query: "select u.id,u.mbr_id,u.parent_id,u.chatExchanges from members u where u.being=@being",
 			parameters: [
 				{
 					name: "@being",
 					value: 'chat'
 				},
 				{
-					name: "@mbr_id",
-					value: this.#mbr_id
+					name: "@id",
+					value: this.core.id
 				}
 			]
 		})
 	}
+	async getItem(_id){
+		return await this.datamanager.getItem(_id)
+	}
 	async getQuestions(){
-		return await this.Datamanager.find({
+		return await this.#Datamanager.getItems({
 			query: "select u.id,u.mbr_id from members u where u.being=@type and u.mbr_id=@id",
 			parameters: [
 				{
@@ -63,8 +79,8 @@ class Dataservices{
 			]
 		})
 	}
-	async commitRequest(_data={}){
-		return _data
+	async pushItem(_data){
+		return await this.datamanager.pushItem(_data)
 	}
 }
 //	exports
