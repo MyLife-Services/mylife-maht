6e225f057148b3e7b5198dc2de5cf7fb563740e0
Erik Jespersen <mookse@gmail.com>
2025-03-16 23:11:14 -0400

    20250316 @Mookse - versioning

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import chalk from 'chalk'
 /* local service imports */
 import SystemAvatar from './inc/js/mylife-factory.mjs'
 /** variables **/
-const version = '0.0.32'
+const version = '0.0.33'
 const app = new Koa()
 const port = process.env.PORT
 	?? '3000'
