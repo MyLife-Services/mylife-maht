534a1e0af7ab33462c2268f19733af465c408657
Erik Jespersen <mookse@gmail.com>
2026-03-16 20:20:24 -0400

    20260316 @Mookse - logic fix in mFetchStart.missions

--- views/assets/js/guests.mjs
+++ views/assets/js/guests.mjs
@@ -222,9 +222,9 @@ function mCreateChallengeElement(){
  */
 async function mFetchStart(){
     const isSignedUp = await mGlobals.datamanager.signupStatus()
-    if(!mGlobals.isGuid(mMissionId))
-        return
-    const missions = await mGlobals.datamanager.availableMissions()
+    let missions
+    if(mGlobals.isGuid(mMissionId))
+        missions = await mGlobals.datamanager.availableMissions()
     !isSignedUp
         ? hide(signupSuccess)
         : mSignupSuccess()
@@ -251,6 +251,7 @@ async function mFetchStart(){
     return {
         input,
         messages,
+        missions,
     }
 }
 /**
